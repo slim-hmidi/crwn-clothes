b0ae479bdb6af1cea1134e3653badac5ff7b8169
Slim Hmidi <slim.hmidi1@gmail.com>
2019-10-21 12:09:52 +0200

    update app with shop page, collection preview and collection item component

---
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+import "./collection-preview.styles.scss";
+import CollectionItem from "../collection-item/collection-item.component";
+
+
+const CollectionPreview = ({ items, title }) => (
+  <div className="collection-preview">
+    <h1 className="title">
+      {title.toUpperCase()}
+    </h1>
+    <div className="preview">
+      {
+        items
+          .filter((item, idx) => idx < 4)
+          .map(({ id, ...otherComponentProps }) => (
+            <CollectionItem key={id} {...otherComponentProps} />
+          ))
+      }
+    </div>
+  </div>
+)
+
+export default CollectionPreview
