e09f32e4ba81e29883771019e914bf9e47ef11eb
Slim Hmidi <slim.hmidi1@gmail.com>
2019-10-22 22:03:31 +0200

    implement firebase utils

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import Header from './components/header/header.component';
-import { auth } from './firebase/firebase.utils';
+import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 
 class App extends Component {
   constructor() {
@@ -19,9 +19,23 @@ class App extends Component {
 
   unsubScribeFromAuth = null;
   componentDidMount() {
-    this.unsubScribeFromAuth = auth.onAuthStateChanged(user => (
-      this.setState({ currentUser: user })
-    ))
+    this.unsubScribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth)
+        userRef.onSnapshot(snapshot => {
+          this.setState({
+            currentUser: {
+              id: snapshot.id,
+              ...snapshot.data()
+            }
+          })
+        })
+      } else {
+        this.setState({ currentUser: userAuth })
+      }
+
+
+    })
   }
 
   componentWillUnmount() {
