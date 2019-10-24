24378b712d30b26737b1e3e2c5ed65b7b3a77a81
Slim Hmidi <slim.hmidi1@gmail.com>
2019-10-24 18:47:37 +0200

    create respective redux flow for directory/shop

---
+++ src/components/collections-overview/collections-overview.component.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { createStructuredSelector } from "reselect"
+import { connect } from "react-redux"
+import CollectionPreview from "../collection-preview/collection-preview.component";
+import { selectCollectionsForPreview } from "../../redux/shop/shop.selectors"
+
+
+import "./collections-overview.styles.scss"
+
+
+const CollectionsOverview = ({ collections }) => (
+  <div className='collections-overview'>
+    {
+      collections.map(({ id, ...otherCollectionProps }) => {
+        return (<CollectionPreview key={id} {...otherCollectionProps} />);
+      })
+    }
+  </div>
+)
+
+const mapStateToProps = createStructuredSelector({
+  collections: selectCollectionsForPreview
+})
+
+export default connect(mapStateToProps)(CollectionsOverview)
