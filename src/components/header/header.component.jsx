ee287bae63e12eedf7801a817781066d99ae230e
Slim Hmidi <slim.hmidi1@gmail.com>
2019-10-29 19:12:26 +0100

    add withSpinner and add shop data to the firebase

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,50 +1,46 @@
-import React from "react";
-import { connect } from "react-redux"
-import { createStructuredSelector } from "reselect"
-import { ReactComponent as Logo } from "../../assets/crown.svg"
-import { auth } from "../../firebase/firebase.utils";
-import CartIcon from "../cart-icon/cart-icon.component"
-import CartDropDown from "../cart-dropdown/cart-dropdown.component"
-import { selectCartHidden } from "../../redux/cart/cart.selectors";
-import { selectCurrentUser } from "../../redux/user/user.selectors"
+import React from 'react';
+import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
 
-import { HeaderContainer, LogoContainer, OptionContainer, OptionLink } from "./header.styles"
+import { auth } from '../../firebase/firebase.utils';
+import CartIcon from '../cart-icon/cart-icon.component';
+import CartDropdown from '../cart-dropdown/cart-dropdown.component';
+import { selectCartHidden } from '../../redux/cart/cart.selectors';
+import { selectCurrentUser } from '../../redux/user/user.selectors';
 
+import { ReactComponent as Logo } from '../../assets/crown.svg';
+
+import {
+  HeaderContainer,
+  LogoContainer,
+  OptionsContainer,
+  OptionLink
+} from './header.styles';
 
 const Header = ({ currentUser, hidden }) => (
   <HeaderContainer>
-    <LogoContainer to="/">
-      <Logo className="logo" />
+    <LogoContainer to='/'>
+      <Logo className='logo' />
     </LogoContainer>
-    <OptionContainer>
-      <OptionLink to="/shop">
-        SHOP
-      </OptionLink>
-      <OptionLink to="/contact">
-        CONTACT
-      </OptionLink>
-      {
-        currentUser ?
-          <OptionLink as='div' onClick={() => auth.signOut()}>
-            SIGN OUT
-          </OptionLink>
-          :
-          <OptionLink to="/signin">
-            SIGN IN
-          </OptionLink>
-      }
+    <OptionsContainer>
+      <OptionLink to='/shop'>SHOP</OptionLink>
+      <OptionLink to='/shop'>CONTACT</OptionLink>
+      {currentUser ? (
+        <OptionLink as='div' onClick={() => auth.signOut()}>
+          SIGN OUT
+        </OptionLink>
+      ) : (
+          <OptionLink to='/signin'>SIGN IN</OptionLink>
+        )}
       <CartIcon />
-    </OptionContainer>
-    {
-      hidden ? null : <CartDropDown />
-    }
-
+    </OptionsContainer>
+    {hidden ? null : <CartDropdown />}
   </HeaderContainer>
-)
+);
 
 const mapStateToProps = createStructuredSelector({
   currentUser: selectCurrentUser,
   hidden: selectCartHidden
-})
+});
 
 export default connect(mapStateToProps)(Header);
--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,13 +1,46 @@
-import React from "react";
+import React, { Component } from "react";
 import { Route } from "react-router-dom"
+import { connect } from "react-redux"
 import CollectionsOverview from "../../components/collections-overview/collections-overview.component"
 import CollectionPage from "../collection/collection.component"
+import { firestore, convertCollectionsSnapshotToMap } from "../../firebase/firebase.utils"
+import { updateCollections } from "../../redux/shop/shop.actions"
+
+import WithSpinner from "../../components/with-spinnner/with-spinner.component"
+
+const CollectionsOverviewWithSpinner = WithSpinner(CollectionsOverview);
+const CollectionPageWithSpinner = WithSpinner(CollectionPage)
 
 
-const ShopPage = ({ match }) => (
-  <div className="shop-page">
-    <Route exact path={`${match.path}`} component={CollectionsOverview} />
-    <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
-  </div>
-)
-export default ShopPage;
+class ShopPage extends Component {
+  state = {
+    loading: true
+  }
+
+  unsbscribeFromSnapshot = null;
+
+  componentDidMount() {
+    const { updateCollection } = this.props
+    const collectionRef = firestore.collection('collections');
+    fetch('https://firestore.googleapis.com/v1/projects/crwn-db/databases/(default)/documents')
+    collectionRef.get().then((snapshot) => {
+      const collectionsMap = convertCollectionsSnapshotToMap(snapshot)
+      updateCollection(collectionsMap)
+      this.setState({ loading: false })
+    })
+  }
+  render() {
+    const { match } = this.props;
+    const { loading } = this.state;
+    return (<div className="shop-page">
+      <Route exact path={`${match.path}`} render={(props) => <CollectionsOverviewWithSpinner isLoading={loading} {...props} />} />
+      <Route path={`${match.path}/:collectionId`} render={(props) => <CollectionPageWithSpinner isLoading={loading} {...props} />} />
+    </div>
+    )
+  }
+}
+
+const mapDispatchToProps = (dispatch) => ({
+  updateCollection: collectionsMap => dispatch(updateCollections(collectionsMap))
+})
+export default connect(null, mapDispatchToProps)(ShopPage);
